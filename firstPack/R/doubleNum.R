a03554d27a26a7ab3a333fa08058435293f1e41d
lancezlin <linzhengzhong@icloud.com>
2015-06-30 19:49:10 -0700

    add one more function to sqr a number

--- firstPack/R/doubleNum.R
+++ firstPack/R/doubleNum.R
@@ -1,10 +1,14 @@
 #' first function
 #' to double the number
 #' @param x is a number
-#' @examples doubleNum(x)
-#' =9
 #' @author lancezlin
+#' @examples doubleNum(x) = 9
+#' @examples sqrNum(x) = 9
 #' 
 doubleNum <- function(x){
   return (2*x)
+}
+
+sqrNum <- function(x){
+  return (x**2)
 }
