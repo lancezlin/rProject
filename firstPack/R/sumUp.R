fa53b1ec244df20a6d233ee846214e7738a23213
lancezlin <linzhengzhong@icloud.com>
2015-06-30 19:57:29 -0700

    second function in r to sum up a integer

---
+++ firstPack/R/sumUp.R
@@ -0,0 +1,12 @@
+#' second function series in R
+#'
+#' @param y is an integer
+#' @author lancezlin
+
+sumUp <- function(y){
+  total = 0
+  for (i in range(y)){
+    total = total + i
+  }
+  return (total)
+}
